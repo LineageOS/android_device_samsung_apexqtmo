c9c55b325a824e401101fafd2effbda6d801d5e0
Dave Daynard <nardholio@gmail.com>
2015-10-04 19:42:59 -0400

    Apexqtmo: Lollipop bringup
    with apologies to jrior, noobnl, and others for mashing their work
    together without history
    
    Change-Id: Ia4d35c1ae77bcdab9d6599e1ac5e40f6ae5f750a

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -1,6 +1,14 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := fstab.qcom
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/fstab.qcom
+LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.target.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
@@ -9,33 +17,9 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := 50bluetooth
+LOCAL_MODULE       := init.bt.sh
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/50bluetooth
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init.d
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := 60compass
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/60compass
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init.d
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := 60sensors
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/60sensors
-LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init.d
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
-LOCAL_MODULE       := wifimac.sh
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/wifimac.sh
-LOCAL_MODULE_PATH  := $(TARGET_OUT)/bin
+LOCAL_SRC_FILES    := etc/init.bt.sh
+LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)
 include $(BUILD_PREBUILT)
